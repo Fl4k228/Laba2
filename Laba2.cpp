932ca630c72b2240f8a85f7efcc386454792f924
Fl4k228 <valovg79@gmail.com>
2024-09-25 01:58:36 +0700

    Add files via upload

---
+++ Laba2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double t[9] = { 0, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5 };
+    double g = 9.81;
+    double m = 75.0;
+    double k = 8.0;
+    double v[9];
+
+    for (size_t i = 0; i < size(t); i++)
+    {
+        v[i] = sqrt(g * m / k) * tanh(t[i] * sqrt(g * k / m));
+        cout << t[i] << "          " << v[i] << endl;
+    }
+}
