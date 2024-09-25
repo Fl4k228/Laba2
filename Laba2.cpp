7dc358d0f01c41ce1eeed7e8b7dfb697e9a02d53
Fl4k228 <valovg79@gmail.com>
2024-09-25 17:39:12 +0700

    Update Laba2.cpp

--- Laba2.cpp
+++ Laba2.cpp
@@ -1,17 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
-    double t[9] = { 0, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5 };
-    double g = 9.81;
-    double m = 75.0;
-    double k = 8.0;
-    double v[9];
+  double t[] = { 0, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5 };
+  double g = 9.81;
+  double m = 75.0;
+  double k = 8.0;
+  double v[9];
+  int a = 0;
 
-    for (size_t i = 0; i < size(t); i++)
-    {
-        v[i] = sqrt(g * m / k) * tanh(t[i] * sqrt(g * k / m));
-        cout << t[i] << "          " << v[i] << endl;
-    }
+  while (a < size(t))
+  {
+    cout.precision(4);
+    v[a] = sqrt(g * m / k) * tanh(t[a] * sqrt(g * k / m));
+    cout << t[a] << "          " << v[a] << endl;
+    a++;
+  }
 }
